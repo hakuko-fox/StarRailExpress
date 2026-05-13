5e49af039c3430346243059ae22dd872b4e16e14
wifi-left <ysy1679682327@outlook.com>
2026-05-13 17:20:55 +0800

    修复客户端名称渲染问题

--- src/main/java/org/agmas/noellesroles/mixin/client/roles/morphling/MorphlingRoleNameRendererMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/client/roles/morphling/MorphlingRoleNameRendererMixin.java
@@ -4,16 +4,17 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.wifi.starrailexpress.client.SREClient;
 import io.wifi.starrailexpress.client.gui.RoleNameRenderer;
-import net.fabricmc.loader.impl.util.log.Log;
-import net.fabricmc.loader.impl.util.log.LogCategory;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.scores.PlayerTeam;
+
 import org.agmas.harpymodloader.component.WorldModifierComponent;
 import org.agmas.noellesroles.ConfigWorldComponent;
 import org.agmas.noellesroles.client.NoellesrolesClient;
-import org.agmas.noellesroles.component.ModComponents;
 import org.agmas.noellesroles.game.roles.killer.morphling.MorphlingPlayerComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,6 +24,12 @@ import java.util.UUID;
 
 @Mixin(RoleNameRenderer.class)
 public abstract class MorphlingRoleNameRendererMixin {
+
+    private static Component getDisplayName$PlayerInfo(PlayerInfo playerInfo) {
+        MutableComponent mutableComponent = PlayerTeam.formatNameForTeam(playerInfo.getTeam(),
+                Component.literal(playerInfo.getProfile().getName()));
+        return (mutableComponent);
+    }
 
     private static UUID getShuffledTarget(Player player) {
         var worldModifiers = WorldModifierComponent.KEY.get(player.level());
@@ -44,32 +51,23 @@ public abstract class MorphlingRoleNameRendererMixin {
 
     @WrapOperation(method = "renderHud", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getDisplayName()Lnet/minecraft/network/chat/Component;"))
     private static Component renderRoleHud(Player instance, Operation<Component> original) {
-
+        if (instance == null)
+            return original.call(instance);
         if (getShuffledTarget(instance) != null) {
             return Component.literal("??!?!").withStyle(ChatFormatting.OBFUSCATED);
         }
         if (instance.isInvisible()) {
             return Component.literal("");
         }
-        var deathPenalty = ModComponents.DEATH_PENALTY.get(Minecraft.getInstance().player);
-        boolean hasPenalty = false;
-        if (deathPenalty != null)
-            hasPenalty = deathPenalty.hasPenalty();
-        // boolean hasPenalty =
-        // ModComponents.DEATH_PENALTY.get(Minecraft.getInstance().player).hasPenalty();
-
-        if (hasPenalty) {
-            // return
-            // Component.translatable("message.noellesroles.penalty.limit.player_name");
-        }
-        if ((MorphlingPlayerComponent.KEY.get(instance)).getMorphTicks() > 0) {
-            if (instance.level().getPlayerByUUID(MorphlingPlayerComponent.KEY.get(instance).disguise) != null) {
-                return instance.level().getPlayerByUUID((MorphlingPlayerComponent.KEY.get(instance)).disguise)
-                        .getDisplayName();
+        var mocca = MorphlingPlayerComponent.KEY.get(instance);
+        if ((mocca).getMorphTicks() > 0) {
+            PlayerInfo targetInfo = SREClient.PLAYER_ENTRIES_CACHE.getOrDefault(mocca.disguise, null);
+            if (targetInfo != null && targetInfo.getProfile() != null && targetInfo.getProfile().getId() != null) {
+                return getDisplayName$PlayerInfo(targetInfo);
             } else {
-                Log.info(LogCategory.GENERAL, "Morphling disguise is null!!!");
+                // Log.info(LogCategory.GENERAL, "Morphling disguise is null!!!");
             }
-            if (MorphlingPlayerComponent.KEY.get(instance).disguise.equals(Minecraft.getInstance().player.getUUID())) {
+            if (mocca.disguise.equals(Minecraft.getInstance().player.getUUID())) {
                 return Minecraft.getInstance().player.getDisplayName();
             }
         }
