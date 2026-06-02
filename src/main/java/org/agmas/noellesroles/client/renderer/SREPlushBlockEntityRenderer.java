c3cc076e7538e241e9579818be0e87dbd5efd32e
wifi-left <ysy1679682327@outlook.com>
2026-06-02 17:19:21 +0800

    1f instead of 255f

--- src/main/java/org/agmas/noellesroles/client/renderer/SREPlushBlockEntityRenderer.java
+++ src/main/java/org/agmas/noellesroles/client/renderer/SREPlushBlockEntityRenderer.java
@@ -19,18 +19,23 @@ public class SREPlushBlockEntityRenderer<T extends BlockEntity> implements Block
     }
 
     @Override
-    public void render(T blockEntity, float delta, PoseStack poseStack, MultiBufferSource consumer, int light, int overlay) {
+    public void render(T blockEntity, float delta, PoseStack poseStack, MultiBufferSource consumer, int light,
+            int overlay) {
         poseStack.pushPose();
         var squish = blockEntity instanceof SREPlushBlockEntity plushie ? plushie.squash : 0;
         var lastSquish = squish * 3;
-        var squash = (float)Math.pow((double)1.0F - (double)1.0F / ((double)1.0F + Mth.lerp((double)delta, lastSquish, squish)), (double)2.0F);
+        var squash = (float) Math.pow(
+                (double) 1.0F - (double) 1.0F / ((double) 1.0F + Mth.lerp((double) delta, lastSquish, squish)),
+                (double) 2.0F);
         poseStack.scale(1, 1F - squash, 1);
         poseStack.translate(0.5, 0, 0.5);
         poseStack.scale(1F + squash / 2F, 1, 1F + squash / 2F);
         poseStack.translate(-0.5, 0, -0.5);
         var state = blockEntity.getBlockState();
         var bakedModel = this.renderer.getBlockModel(state);
-        this.renderer.getModelRenderer().renderModel(poseStack.last(), consumer.getBuffer(ItemBlockRenderTypes.getRenderType(state, false)), state, bakedModel, 255F, 255F, 255F, light, overlay);
+        this.renderer.getModelRenderer().renderModel(poseStack.last(),
+                consumer.getBuffer(ItemBlockRenderTypes.getRenderType(state, false)), state, bakedModel, 1F, 1F, 1F,
+                light, overlay);
         poseStack.popPose();
     }
 
