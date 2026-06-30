0df945f168b2c7187da383fefe242ad09a62e9c5
wifi-left <ysy1679682327@outlook.com>
2026-06-30 19:06:52 +0800

    true

--- src/main/java/io/wifi/starrailexpress/SREConfig.java
+++ src/main/java/io/wifi/starrailexpress/SREConfig.java
@@ -236,7 +236,7 @@ public class SREConfig implements ConfigData {
     public boolean forensicShowCorpseFacing = true;
     @ConfigEntry.Category(value = "forensic")
     @ConfigSync(shouldSync = true)
-    public boolean forensicBloodTrail = false;
+    public boolean forensicBloodTrail = true;
     @ConfigEntry.Category(value = "forensic")
     @ConfigSync(shouldSync = true)
     public int forensicBloodTrailSeconds = 40;
