14193a5db0d1a7706bac968822694cb5ff17f4f8
wifi-left <ysy1679682327@outlook.com>
2026-07-03 22:40:02 +0800

    添加flag

--- src/main/java/io/wifi/starrailexpress/api/OriginalRole.java
+++ src/main/java/io/wifi/starrailexpress/api/OriginalRole.java
@@ -7,6 +7,7 @@ public class OriginalRole extends NormalRole {
     public OriginalRole(ResourceLocation identifier, int color, boolean isInnocent, boolean canUseKiller,
             MoodType moodType, int maxSprintTime, boolean canSeeTime) {
         super(identifier, color, isInnocent, canUseKiller, moodType, maxSprintTime, canSeeTime);
+        this.addFlag("inner.original");
     }
     
 }
