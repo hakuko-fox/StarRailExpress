7edf155b221858a0352b77f8311a94c2b9db3fe8
wifi-left <ysy1679682327@outlook.com>
2026-07-08 13:11:17 +0800

    fix

--- src/main/java/io/wifi/starrailexpress/mixin/SREMixinPlugin.java
+++ src/main/java/io/wifi/starrailexpress/mixin/SREMixinPlugin.java
@@ -25,11 +25,6 @@ public class SREMixinPlugin implements IMixinConfigPlugin {
             if (mixinClassName.contains("io.wifi.starrailexpress.mixin.compat.sodium_extra"))
                 return false;
         }
-        
-        if (!FabricLoader.getInstance().isModLoaded("handheldmoon")
-                && (mixinClassName.contains("io.wifi.starrailexpress.mixin.compat.handheldmoon."))) {
-            return false;
-        }
         if (!FabricLoader.getInstance().isModLoaded("sodium")
                 && (mixinClassName.contains("net.exmo.mixin.client.side")
                         || mixinClassName.contains("io.wifi.starrailexpress.mixin.compat.sodium."))) {
