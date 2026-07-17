d9b2c89e585387bcc56c2ce970dd0ca4735ab132
wifi-left <ysy1679682327@outlook.com>
2026-07-17 21:02:32 +0800

    1

--- src/main/java/org/agmas/harpymodloader/mixin/PlayerWeightResetMixin.java
+++ src/main/java/org/agmas/harpymodloader/mixin/PlayerWeightResetMixin.java
@@ -1,8 +1,6 @@
 package org.agmas.harpymodloader.mixin;
 
-import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
 import org.agmas.harpymodloader.modded_murder.PlayerRoleWeightManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerList.class)
 public class PlayerWeightResetMixin {
-    //
 
     @Inject(method = "remove", at = @At("HEAD"))
     public void onPlayerDisconnect(ServerPlayer player,
