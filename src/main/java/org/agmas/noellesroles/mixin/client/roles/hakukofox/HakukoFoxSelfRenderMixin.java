4bbd940e9fb80bab2e80af397336c7e318cdcbde
Hakukofox <hakukofox@gmail.com>
2026-07-25 21:26:48 +0800

    Fix POV body not rendering: remove isDetached override for Snow Fox

--- src/main/java/org/agmas/noellesroles/mixin/client/roles/hakukofox/HakukoFoxSelfRenderMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/client/roles/hakukofox/HakukoFoxSelfRenderMixin.java
@@ -3,10 +3,8 @@ package org.agmas.noellesroles.mixin.client.roles.hakukofox;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.world.entity.animal.Fox;
 import org.agmas.noellesroles.client.HakukoFoxDisguiseRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,10 +15,6 @@ public abstract class HakukoFoxSelfRenderMixin {
     @WrapOperation(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;isDetached()Z"))
     private boolean noellesroles$renderSelfWhileDisguised(Camera camera, Operation<Boolean> original) {
         if (original.call(camera)) {
-            Minecraft mc = Minecraft.getInstance();
-            if (mc.cameraEntity instanceof Fox fox && fox.getVariant() == Fox.Type.SNOW) {
-                return false;
-            }
             return true;
         }
         return camera.getEntity() instanceof AbstractClientPlayer player
